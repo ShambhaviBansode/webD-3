0545730b47faae95b8168747bf8980c79e2439ea
ShambhaviBansode <161575637+ShambhaviBansode@users.noreply.github.com>
2024-04-02 23:04:01 +0530

    Add files via upload

---
+++ Exp5.js
@@ -0,0 +1,12 @@
+let num1 = prompt("Enter number 1: ");
+let num2 = prompt("Enter number 2: ");
+
+if(num1 > num2){
+    alert("Number 1 is greater");
+}
+if(num1 < num2){
+    alert("Number 2 is greater");
+}
+if(num1 === num2){
+    alert("Both are equal");
+}
---
+++ Exp6 A.js
@@ -0,0 +1,12 @@
+let i =1;
+let sum =0;
+while(i<=5){
+   var a = prompt("Enter an integer");
+    i++;
+    let b = parseInt(a);
+sum = sum + b;
+
+}
+document.write("Sum of numbers using while loop =",sum);
+
+
